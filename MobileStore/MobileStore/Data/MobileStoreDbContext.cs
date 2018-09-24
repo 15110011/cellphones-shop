41cf917d2a4f895d0877163cea5ee7ab68877ee4
15110092-Nguyễn Hữu Nhân <32167201+15110092-NguyenHuuNhan@users.noreply.github.com>
2018-09-24 23:43:31 +0700

    MobileDBcontext

--- MobileStore/MobileStore/Data/MobileStoreDbContext.cs
+++ MobileStore/MobileStore/Data/MobileStoreDbContext.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
-
-
+using MobileStore.Data.Models;
 
 namespace MobileStore
 {
     public partial class MobileStoreDbContext : DbContext
     {
+        public MobileStoreDbContext(DbContextOptions<MobileStoreDbContext> options) : base(options)
+        {
+        }
 
-
+        public DbSet<Phone> Drinks { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
     }
 }
