7a4260f9fed2cf30f47a5b7e678455c0e0629d2b
Duc Anh <tdanhdev@gmail.com>
2018-12-13 20:14:37 +0700

    feat: upload image

--- MobileStore/MobileStore/Areas/Admin/Controllers/PhonesController.cs
+++ MobileStore/MobileStore/Areas/Admin/Controllers/PhonesController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -13,12 +16,14 @@ namespace MobileStore.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class PhonesController : Controller
-    { 
+    {
         private readonly MobileStoreDbContext _context;
+        private readonly IHostingEnvironment __appEnvironment;
 
-        public PhonesController(MobileStoreDbContext context)
+        public PhonesController(MobileStoreDbContext context,IHostingEnvironment appEnvironment)
         {
             _context = context;
+            __appEnvironment = appEnvironment;
         }
 
         // GET: Admin/Phones
@@ -57,16 +62,44 @@ namespace MobileStore.Areas.Admin.Controllers
         // POST: Admin/Phones/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("PhoneId,Name,ShortDescription,Price,ImageUrl,ImageThumbnailUrl,IsPreferredPhone,CategoryId")] Phone phone)
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("PhoneId,Name,ShortDescription,Price,ImageUrl,ImageThumbnailUrl,IsPreferredPhone,CategoryId")] Phone phone, IFormFile file)
         {
+
+            if (file == null || file.Length == 0) return Content("file not selected");
             if (ModelState.IsValid)
             {
                 _context.Add(phone);
+
+                //< get Path >
+
+                string pathRoot = __appEnvironment.WebRootPath;
+
+                string pathToImages = pathRoot + "\\images\\" + file.FileName;
+                string imageUrl = "\\images\\" + file.FileName;
+                //</ get Path >
+
+                //< Copy File to Target >
+                
+
+                using (var stream = new FileStream(pathToImages, FileMode.Create))
+
+                {
+                    await file.CopyToAsync(stream);
+                }
+                //</ Copy File to Target >
+                if (pathToImages != null)
+                {
+                    phone.ImageUrl = imageUrl;
+                }
+                //< output >
+
+                ViewData["FilePath"] = imageUrl;
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
+            
             ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", phone.CategoryId);
             return View(phone);
         }
@@ -93,18 +126,43 @@ namespace MobileStore.Areas.Admin.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("PhoneId,Name,ShortDescription,Price,ImageUrl,ImageThumbnailUrl,IsPreferredPhone,CategoryId")] Phone phone)
+        public async Task<IActionResult> Edit(int id, [Bind("PhoneId,Name,ShortDescription,Price,ImageUrl,ImageThumbnailUrl,IsPreferredPhone,CategoryId")] Phone phone, IFormFile file)
         {
             if (id != phone.PhoneId)
             {
                 return NotFound();
             }
+            if (file == null || file.Length == 0) return Content("file not selected");
 
             if (ModelState.IsValid)
             {
                 try
                 {
                     _context.Update(phone);
+                    //< get Path >
+
+                    string pathRoot = __appEnvironment.WebRootPath;
+
+                    string pathToImages = pathRoot + "\\images\\" + file.FileName;
+                    string imageUrl = "\\images\\" + file.FileName;
+                    //</ get Path >
+
+                    //< Copy File to Target >
+
+
+                    using (var stream = new FileStream(pathToImages, FileMode.Create))
+
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                    //</ Copy File to Target >
+                    if (pathToImages != null)
+                    {
+                        phone.ImageUrl = imageUrl;
+                    }
+                    //< output >
+
+                    ViewData["FilePath"] = imageUrl;
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
@@ -158,5 +216,60 @@ namespace MobileStore.Areas.Admin.Controllers
         {
             return _context.Phones.Any(e => e.PhoneId == id);
         }
+
+         //1.Load
+
+        //public IActionResult Upload_Image()
+
+        //{
+
+        //    //--< Upload Form >--
+
+        //    return View();
+
+        //    //--</ Upload Form >--
+
+        //}
+       // [HttpPost] //Postback
+        //public async Task<IActionResult> Upload_Image([FromForm]IFormFile file)
+
+        //{
+
+        //    //--------< Upload_ImageFile() >--------
+
+        //    //< check >
+
+        //    if (file == null || file.Length == 0) return Content("file not selected");
+
+        //    //</ check >
+
+
+
+        //    //< get Path >
+
+        //    string pathRoot = __appEnvironment.WebRootPath;
+
+        //    string pathToImages = pathRoot + "\\images\\" + file.FileName;
+
+        //    //</ get Path >
+
+        //    //< Copy File to Target >
+
+        //    using (var stream = new FileStream(pathToImages, FileMode.Create))
+
+        //    {
+        //        await file.CopyToAsync(stream);
+        //    }
+        //    //</ Copy File to Target >
+
+        //    //< output >
+
+        //    ViewData["FilePath"] = pathToImages;
+
+        //    return View("Create");
+        //    //</ output >
+        //    //--------</ Upload_ImageFile() >--------
+        //}
     }
 }
+
